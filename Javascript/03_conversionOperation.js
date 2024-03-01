3409b2d9cb97845d38cdbdfbfb73fabb1d55cc02
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-03-01 19:17:13 +0000

    conversion of datatypes

---
+++ Javascript/03_conversionOperation.js
@@ -0,0 +1,47 @@
+/*
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+// Typecasting
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) // shows NaN(not a number)
+
+*/
+
+
+// let a = null
+// let value1 = Number(a)
+// console.log(typeof a);
+// console.log(value1);
+
+
+
+// let b = undefined
+// let value2 = Number(b)
+// console.log(typeof b);
+// console.log(value2);
+
+
+// let c = undefined
+// let value3 = Number(c)
+// console.log(typeof c);
+// console.log(value3);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "abc" => true
+
+let number = 33
+let str = String(number)
+
+console.log(str);
+console.log(typeof str);
