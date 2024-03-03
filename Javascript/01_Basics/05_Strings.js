872b6cec6b17203062556bce0f46d03b11a64bd2
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-03-03 13:27:49 +0000

    Strings

---
+++ Javascript/01_Basics/05_Strings.js
@@ -0,0 +1,47 @@
+const name = "rahul"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+// String interpolation
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const newGame = new String ('rahul-n-com') // creates a new String object
+
+// console.log(typeof name)
+// console.log(typeof newGame)
+
+// console.log(newGame[0])
+// console.log(newGame.__proto__)
+
+// String methods
+
+// console.log(newGame.length)
+// console.log(newGame.toUpperCase());
+
+// console.log(newGame.charAt(2))
+// console.log(newGame.indexOf('h'))
+
+// console.log(newGame.substring(0,4))
+
+// console.log(newGame.slice(-5,6))
+
+// const newString = "    rahul     "
+
+// console.log(newString)
+// console.log(newString.trim())
+
+// const mood = "Happy!"
+// console.log(`I feel ${mood.repeat(2)}`)
+
+// const url = "https://mithun.com/mithun%20rajbonuth"
+
+// console.log(url.includes("mithun"))
+// console.log(url.replace('%20','-'))
+
+console.log(newGame.split('-'))
+
+
+
+
