8f9b0a18b24a1bf9b77358b9e5fd1066a2b1ef43
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-03-07 21:08:19 +0000

    for loop

---
+++ Javascript/05_iterations/one.js
@@ -0,0 +1,45 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is best number")
+//     }
+//     console.log(element)
+// }
+
+
+// for (let i = 1; i <= 10 ; i++) {
+//     console.log(`Outer Loop value: ${i}`)
+//     for (let j = 1; j <=10 ; j++) {
+//         // console.log(`Inner Loop value ${j} and outer loop ${i}`)
+//         console.log(`${i} * ${j}  = ${i*j}`)
+//     }
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+// }
+
+// Break and continue
+
+for (let i = 0; i <=20 ; i++) {
+    if(i == 5){
+        console.log("Detected 5")
+        break;
+    }
+    console.log(`Value of index is ${i}`);
+    
+}
+
+for (let i = 0; i <=20 ; i++) {
+    if(i == 5){
+        console.log("Detected 5")
+        continue;
+    }
+    console.log(`Value of index is ${i}`);
+    
+}
