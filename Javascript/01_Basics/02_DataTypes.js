21cd0d6469f0f7b46794b12a2039a67b3658e5b3
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-08-24 13:47:42 +0000

    Updated DataTypes

--- Javascript/01_Basics/02_DataTypes.js
+++ Javascript/01_Basics/02_DataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; // treats all JS code as newer version
 
-// alert("hello") // we are using node.js and not browser
+// alert("hello") // we are using node.js and not browser so it throws an error here
 
 // Code readability should be high
 
@@ -17,11 +17,27 @@ let isLoggedIn = false
 // BigInt 
 // string = ""
 // boolean =>true/false
-// null : standalone value
-// undefined => no value has been assigned
+// null : standalone value [indicates the absence of an object]
+// undefined => no value has been assigned [indicates the absence of a value]
 // symbol => uniqueness
 
 //object
 
 console.log(typeof undefined) // undefined
 console.log(typeof null); // object
+
+
+console.log(typeof NaN);
+
+// Infinity in JS
+
+console.log(42 / 0); // Infinity
+console.log(42 / -0); //-Infinity
+
+
+console.log(typeof Infinity);
+
+console.log(Infinity + 1);   // Outputs: Infinity
+console.log(Infinity * 2);   // Outputs: Infinity
+console.log(Infinity - Infinity); // Outputs: NaN
+console.log(Infinity / Infinity); // Outputs: NaN
