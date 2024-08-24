21c6bf744eab8b1011fc9f219130488cdd13a3cd
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-08-24 13:22:34 +0000

    updated 01_variables

--- Javascript/01_Basics/01_variables.js
+++ Javascript/01_Basics/01_variables.js
@@ -1,9 +1,9 @@
 const accountId = 144553
 let accountEmail = "rahulnarayan@gmail.com"
 var accountPassword = "12345"
-accountCity = "Kanpur"
+accountCity = "Kanpur"  // has Global scope which can lead to potential conflicts and bugs.
 
-let accountState
+let accountState;
 // accountId = 2 // Assignment to constant variable is not allowed
 
 accountEmail = "rahuliitk@gmail.com"
@@ -14,4 +14,5 @@ accountCity = "Patna"
 Note: Preferred not to use var because of issue in block scope and functional scope
 */
 
+// console.log(accountId, accountEmail, accountPassword, accountCity, accountState)
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
