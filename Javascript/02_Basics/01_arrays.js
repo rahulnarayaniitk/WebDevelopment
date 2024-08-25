76709bf4a99a540491d0cc72ffcda144d70cc57a
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-08-25 13:43:10 +0000

    Modified arrays1

--- Javascript/02_Basics/01_arrays.js
+++ Javascript/02_Basics/01_arrays.js
@@ -1,19 +1,27 @@
 // Arrays
 
 /*
-# JavaScript arrays are resizable and can contain a mix of different data types.
+# JavaScript arrays are "resizable" and can contain a "mix" of different data types.
 # JavaScript array-copy operations create shallow copies.
 
-# Shallow copies : A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made.
-# Deep copies : A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made.
+# Shallow copies : A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may also cause the other object to change too
+
+# Deep copies : A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you can be assured you're not causing the other object to change too
+
 */
 
-const myArr = [0, 1, 2 ,3, 4, 5]
+const myArr = [0, 1, 2 ,3, 4, "wipro"]
+
 // console.log(myArr[5])
 // console.log(typeof myArr)
-const myHeros = ["Shaktiman", "NaagRaj" , "Doga"]
 
-const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr)
+
+// const myHeros = ["Shaktiman", "NaagRaj" , "Doga"]
+// console.log(typeof myHeros);
+
+
+// const myArr2 = new Array(1, 2, 3, 4)
 // console.log(typeof myArr2)
 
 // Array Methods
@@ -25,6 +33,7 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.unshift(9) // adds to the start of an array but it is a time consuming process
 // myArr.shift() // removes element from the start of the array
 
+// console.log(myArr);
 
 // console.log(myArr.includes(5)) // returns boolean to state whether that value exists in the array or not
 // console.log(myArr.indexOf(5)) // returns the index of the entry but if the value doesn't exist in array then it returns -1
