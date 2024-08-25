70727be389816450b4d875f57da66ed6481387d7
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-08-25 00:18:13 +0000

    updated comparisons

--- Javascript/01_Basics/04_comparisons.js
+++ Javascript/01_Basics/04_comparisons.js
@@ -6,7 +6,7 @@
 
 // In order to maintain consistency try to avoid comparing two different data  types.
 
-// console.log("2" > 1)
+// console.log("2" > 1) 
 // console.log("02" > 1)
 
 // console.log(null > 0)
