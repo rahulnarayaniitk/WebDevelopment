b09a3b08140d444eea11976bc725f705618b1a76
Rahul Narayan <106427493+rahulnarayaniitk@users.noreply.github.com>
2024-08-26 16:18:45 +0000

    modified objects

--- Javascript/02_Basics/03_Objects.js
+++ Javascript/02_Basics/03_Objects.js
@@ -1,8 +1,8 @@
 /*  Objects can be declared in 2 ways : 
-        1)as a literal  (not singleton)
-        2)as a constructor
+        1)as a literal  (not singleton) : uses curly brackets: {}
+
+        2)as a constructor : uses the new keyword
          => When we declare object using constructor then it is singleton i.e. it is just one object of its type
-         
          //Object.create
 
 
@@ -28,17 +28,21 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "rahul@chatgpt.com"
+// JsUser.email = "rahul@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "rahul@yahoo.com"
+// JsUser.email = "rahul@yahoo.com"
 // console.log(JsUser)
 
 JsUser.greeting = function(){
     console.log("Hello JS User")
 }
+
+// JsUser.greeting = function(){
+//     return "Hello JS User";
+// }
 console.log(JsUser.greeting())
 
-JsUser.greeting2 = function(){
-    console.log(`Hello JS User, ${this.name}`)
-}
-console.log(JsUser.greeting2())
+// JsUser.greeting2 = function(){
+//     console.log(`Hello JS User, ${this.name}`)
+// }
+// console.log(JsUser.greeting2())
